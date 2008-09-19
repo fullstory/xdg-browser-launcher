3e95302a7ef03654cb5dda9d87bf05d94aa876c6
Kel Modderman <kel@otaku42.de>
2008-09-19 05:00:03 +0000

    Bump iceweasel minimum font defaults up to 11, 9 was a tad tiny.

--- sidux-browser.js
+++ sidux-browser.js
@@ -20,7 +20,7 @@ pref("layout.css.dpi", 0);
 
 // Set sane font sizes
 //
-pref("font.minimum-size.x-western",    9);
+pref("font.minimum-size.x-western",    11);
 pref("font.size.variable.x-western",   12);
 
 // Override browser.tabs.closeButtons
